3602675d737b4db75ee2daada40b721191b071e6
Chochain Lee <chochain@yahoo.com>
2025-12-02 11:02:37 -0800

    ~/java/.../MainActivity.kt shorten local var

--- app/src/main/java/com/google/mediapipe/examples/handlandmarker/MainActivity.kt
+++ app/src/main/java/com/google/mediapipe/examples/handlandmarker/MainActivity.kt
@@ -23,21 +23,18 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.mediapipe.examples.handlandmarker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var main: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        activityMainBinding.navigation.setupWithNavController(navController)
-        activityMainBinding.navigation.setOnNavigationItemReselectedListener {
-            // ignore the reselection
-        }
+        val nav = supportFragmentManager.findFragmentById(
+            R.id.fragment_container
+        ) as NavHostFragment
+        main.navigation.setupWithNavController(nav.navController)
     }
 
     override fun onBackPressed() {
@@ -45,7 +42,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateBackground(lcolor: Int, rcolor: Int) {
-        activityMainBinding.viewLeft.setBackgroundColor(lcolor)
-        activityMainBinding.viewRight.setBackgroundColor(rcolor)
+        main.viewLeft.setBackgroundColor(lcolor)
+        main.viewRight.setBackgroundColor(rcolor)
     }
 }
